2af6452738b22d7b5a815eac920dbdf5754b18a8
yifan <yifan.lim@intactsoftware.com>
2025-05-08 17:07:20 +0100

    test fix firebase hosting issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { resolve } from 'node:path'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
+// For compatibility with Firebase hosting
+const _dirname = new URL('.', import.meta.url).pathname;
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
@@ -16,13 +19,13 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-      '@components': resolve(__dirname, './src/components'),
-      '@types': resolve(__dirname, './src/types'),
-      '@utils': resolve(__dirname, './src/utils'),
-      '@contexts': resolve(__dirname, './src/contexts'),
-      '@assets': resolve(__dirname, './src/assets'),
-      '@styles': resolve(__dirname, './src/styles'),
+      '@': resolve(_dirname, './src'),
+      '@components': resolve(_dirname, './src/components'),
+      '@types': resolve(_dirname, './src/types'),
+      '@utils': resolve(_dirname, './src/utils'),
+      '@contexts': resolve(_dirname, './src/contexts'),
+      '@assets': resolve(_dirname, './src/assets'),
+      '@styles': resolve(_dirname, './src/styles'),
     },
   },
 })
