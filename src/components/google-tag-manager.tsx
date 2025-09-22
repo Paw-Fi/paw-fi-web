4891827be1824e57a6a073d2f780a7cd5b3b1137
yifan <yifan.lim@intactsoftware.com>
2025-09-22 13:05:19 +0100

    test gtag fix

--- src/components/google-tag-manager.tsx
+++ src/components/google-tag-manager.tsx
@@ -1,5 +1,4 @@
 // app/components/GoogleTagManager.jsx
-import { useEffect } from 'react';
 
 declare global {
   interface Window {
@@ -9,24 +8,20 @@ declare global {
 }
 
 export function GoogleTagManager({ gtmId }: { gtmId: string }) {
-  useEffect(() => {
-    // Only run on client side
-    if (typeof window === 'undefined') return;
-    window.dataLayer = window.dataLayer || [];
-    function gtag(command: string, ...args: any[]){window.dataLayer.push([command, ...args]);}
-    window.gtag = gtag;
-    gtag('js', new Date());
-  
-    gtag('config', gtmId);
-  }, [gtmId]);
-
   return (
     <>
-      {/* Preconnect to Google Tag Manager domain for faster connection setup */}
-      <link rel="preconnect" href="https://www.googletagmanager.com" />
-      <link rel="dns-prefetch" href="https://www.google-analytics.com" />
-      {/* Use defer instead of async for better performance */}
-      <script defer src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`}></script>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`}></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtmId}');
+          `,
+        }}
+      />
     </>
   );
 }
